494e24bb73ad9bc697851dd9b0b14a87e02d3bd8
YongCastle <xidid430rr@gmail.com>
2023-05-20 02:52:32 +0900

    0520

--- src/HW/RTL/SOBER_TOP.v
+++ src/HW/RTL/SOBER_TOP.v
@@ -1,9 +1,54 @@
-`timescale 1ns / 1ps
+//
+// Description :
+// SOBER_TOP
+//     |______ Controller.v
+//     |______ preprocess.v
+//     |______ core.v
+//
 
 module SOBER_TOP
 (
+    //========== SYSTEM ==============
+    input wire              clk,
+    input wire              rst_n,
+
+    //========== Memory Controller ==============
+    input wire [7:0]        data_i,
+    input wire              en_i,
+
+    //========== CPU CORE (Not Demanded at this Time)==============
+    output wire [2:0]       state_o,
+
+    //========== VGA ==============
+
+    //========== 7-Segment ============== 
     
+     //========== Switch ==============
+
 );
 
+U_CTR controller
+#(
+
+)
+(
+
+);
+
+U_PRE preprocess
+#(
+
+)
+(
+
+);
+
+U_CORE core
+#(
+
+)
+(
+
+);
 
 endmodule
