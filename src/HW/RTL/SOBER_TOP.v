cf3e764f5c3eb8ea5c7d292bb5e4e39c574f631e
YongCastle <xidid430rr@gmail.com>
2023-05-22 02:47:13 +0900

    hello

--- src/HW/RTL/SOBER_TOP.v
+++ src/HW/RTL/SOBER_TOP.v
@@ -1,15 +1,13 @@
 //
 // Description :
 // SOBER_TOP
-//     |______ Controller.v
+//     |
 //     |______ preprocess.v
 //     |______ core.v
 //
 
 module SOBER_TOP
 (
-    //========== INPUT ======================
-
     //========== SYSTEM =====================
     input wire              CLK,
     input wire              RST_N,
@@ -20,71 +18,67 @@ module SOBER_TOP
 
     //========== Controller ================
     input wire              CORE_RUN_I,
+    output wire             CORE_DONE_O,
 
-    //========== OUTPUT ====================
-
-    //========== Accmulator ====================
+    //========== VGA ====================
     output wire [7:0]       PIXEL_O,
-    output wire             EN_O,
-    //output wire [9:0]       cnt_buf_row,
-    //output wire [9:0]       cnt_buf_col,
-    //========== Controller ================
-    output wire              CORE_DONE_O
-
-    //========== VGA ==============
-
-    //========== 7-Segment ============== 
-    
-     //========== Switch ==============
-
+    output wire             PIXEL_EN_O
 );
 
-reg [7:0]       DATA_0_0_O;
-reg [7:0]       DATA_0_1_O;
-reg [7:0]       DATA_0_2_O;
-reg [7:0]       DATA_1_0_O;
-reg [7:0]       DATA_1_1_O;
-reg [7:0]       DATA_1_2_O;
-reg [7:0]       DATA_2_0_O;
-reg [7:0]       DATA_2_1_O;
-reg [7:0]       DATA_2_2_O;
+wire [7:0]       DATA_0_0;
+wire [7:0]       DATA_0_1;
+wire [7:0]       DATA_0_2;
+wire [7:0]       DATA_1_0;
+wire [7:0]       DATA_1_1;
+wire [7:0]       DATA_1_2;
+wire [7:0]       DATA_2_0;
+wire [7:0]       DATA_2_1;
+wire [7:0]       DATA_2_2;
 
+wire             core_en_w;
 
 preprocess_module U_pre 
 (
+    //======== SYSTEM ========================
     .clk                    (CLK),
     .rst_n                  (RST_N),
+    //======== Controller ===================
     .core_run_i             (CORE_RUN_I),
-    .data_i                 (DATA_I),
-    .data_en_i              (DATA_EN_I),  // input
     .core_done_o            (CORE_DONE_O), 
-    .core_en_o              (CORE_EN_O),
-    .data_0_0_o             (DATA_0_0_O),
-    .data_0_1_o             (DATA_0_1_O),
-    .data_0_2_o             (DATA_0_2_O),
-    .data_1_0_o             (DATA_1_0_O),
-    .data_1_1_o             (DATA_1_1_O),
-    .data_1_2_o             (DATA_1_2_O),
-    .data_2_0_o             (DATA_2_0_O),
-    .data_2_1_o             (DATA_2_1_O),
-    .data_2_2_o             (DATA_2_2_O) //output
+    //======== Memory_Controller ============
+    .data_i                 (DATA_I),
+    .data_en_i              (DATA_EN_I),
+    //======== CORE =========================
+    .data_0_0_o             (DATA_0_0),
+    .data_0_1_o             (DATA_0_1),
+    .data_0_2_o             (DATA_0_2),
+    .data_1_0_o             (DATA_1_0),
+    .data_1_1_o             (DATA_1_1),
+    .data_1_2_o             (DATA_1_2),
+    .data_2_0_o             (DATA_2_0),
+    .data_2_1_o             (DATA_2_1),
+    .data_2_2_o             (DATA_2_2),
+    .core_en_o              (core_en_w)
 );
 
-U_CORE core
+core_module U_CORE
 (
-    .clk                    (CLK),
-    .core_en_i              (CORE_EN_O),
-    .data_0_0_i             (DATA_0_0_O),
-    .data_0_1_i             (DATA_0_1_O),
-    .data_0_2_i             (DATA_0_2_O),
-    .data_1_0_i             (DATA_1_0_O),
-    .data_1_1_i             (DATA_1_1_O),
-    .data_1_2_i             (DATA_1_2_O),
-    .data_2_0_i             (DATA_2_0_O),
-    .data_2_1_i             (DATA_2_1_O),
-    .data_2_2_i             (DATA_2_2_O), // input
+    //======== SYSTEM ========================
+
+    //======== Preprocess ===================
+    .data_0_0_i             (DATA_0_0),
+    .data_0_1_i             (DATA_0_1),
+    .data_0_2_i             (DATA_0_2),
+    .data_1_0_i             (DATA_1_0),
+    .data_1_1_i             (DATA_1_1),
+    .data_1_2_i             (DATA_1_2),
+    .data_2_0_i             (DATA_2_0),
+    .data_2_1_i             (DATA_2_1),
+    .data_2_2_i             (DATA_2_2),
+    .core_en_i              (core_en_w),
+    //======== VGA ===================
     .pixle_o                (PIXEL_O), 
-    .en_o                   (EN_O)              // output
+    .en_o                   (PIXEL_EN_O)       
 );
 
 endmodule
--- src/HW/RTL/preprocess.v
+++ src/HW/RTL/preprocess.v
@@ -1,33 +1,30 @@
-module preprocess
+module preprocess_module
+#(
+    parameter       MAX_ROW = 540,
+    parameter       MAX_COl = 540
+)
 (
     //================================
     //        SYSTEM
     //================================
     input   wire             clk,   
     input   wire             rst_n,
-
     //================================
     //      Controller
     //================================
     //From
     input   wire            core_run_i,
-
     //To
     output  wire            core_done_o,
-
     //================================
     //      Memory Controller   
     //================================
     //From
     input   wire [7:0]      data_i,
     input   wire            data_en_i,   
-    //To
-
     //================================
     //            Core
     //================================
-    //From
-
     //To
     output  wire [7:0]      data_0_0_o,
     output  wire [7:0]      data_0_1_o,
@@ -39,16 +36,7 @@ module preprocess
     output  wire [7:0]      data_2_1_o,
     output  wire [7:0]      data_2_2_o,
 
-
-    
     output  wire            core_en_o,
-    //================================
-    //        7 Segment 
-    //================================
-    //From
- 
-    //To
-    output                   n_segment_up_o,      
 
     //======================================================
     //              DEBUGG
@@ -59,14 +47,12 @@ module preprocess
     output    [9:0]                 cnt_pos_col_o
 );
 
-parameter       MAX_BUF_ROWS = 3;
-parameter       MAX_IMG_COLS = 540;
 parameter       CNT_IMG_COLS = 10;
 
-//8x(3x(540+2))        +2 is for 0
-reg [7:0]     buffer_0[MAX_IMG_COLS+2];     
-reg [7:0]     buffer_1[MAX_IMG_COLS+2];   
-reg [7:0]     buffer_2[MAX_IMG_COLS+2];   
+//8x(3x(540))       
+reg [7:0]     buffer_0[MAX_COL];     
+reg [7:0]     buffer_1[MAX_COL];   
+reg [7:0]     buffer_2[MAX_COL];   
 
 //MEM controller --> BUFFER
 reg [1:0]                                   cnt_buf_row;            //Count row
@@ -84,9 +70,9 @@ always @(posedge clk) begin
     end
     else begin
         if(data_en_i) begin
-            if(cnt_buf_col == MAX_IMG_COLS-1) begin
+            if(cnt_buf_col == MAX_COL-1) begin
                 cnt_buf_col         <= 'd0;
-                if((cnt_buf_row == MAX_BUF_ROWS-1) && (cnt_buf_col == MAX_IMG_COLS-1)) begin
+                if((cnt_buf_row == 2) && (cnt_buf_col == MAX_COL-1)) begin
                     cnt_buf_row         <= 'd0;
                 end
                 else begin
@@ -104,9 +90,9 @@ end
 //Filter POS
 always @(posedge clk) begin
     if(!rst_n) begin
-        buffer_0[MAX_IMG_COLS+2]              <= 'd0;
-        buffer_1[MAX_IMG_COLS+2]              <= 'd0;
-        buffer_2[MAX_IMG_COLS+2]              <= 'd0;
+        buffer_0[MAX_COL]              <= 'd0;
+        buffer_1[MAX_COL]              <= 'd0;
+        buffer_2[MAX_COL]              <= 'd0;
     end
     else begin
         case(cnt_buf_row)
@@ -126,7 +112,7 @@ always @(posedge clk) begin
     end
     else begin
         if(core_run_i) begin
-            if(cnt_pos_col == MAX_IMG_COLS-1) begin
+            if(cnt_pos_col == MAX_COL-1-2) begin
                 cnt_pos_col         <= 'd0;
             end
             else begin
@@ -143,7 +129,7 @@ end
 
 
 // ======== TO Controller ===========================
-assign core_done_o      = (cnt_pos_col == MAX_IMG_COLS-1)? 1 : 0;
+assign core_done_o      = (cnt_pos_col == MAX_COL-2)? 1 : 0;
 
 // ======== TO CORE ===========================
 assign data_0_0_o       = (core_run_i)? buffer_0[cnt_pos_col+0] : 'd0;
@@ -160,8 +146,5 @@ assign data_2_2_o       = (core_run_i)? buffer_2[cnt_pos_col+2] : 'd0;
 
 
 assign core_en_o        = core_run_i;
-
-// ======== TO  ===========================
-assign n_segment_up_o   = (cnt_pos_col == MAX_IMG_COLS-1)? 1 : 0;
 
 endmodule
